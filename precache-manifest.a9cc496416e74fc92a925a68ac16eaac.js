53aa66fc7b428f2423c4f065a34ae6cd6822593e
Code Sandbox <hello@codesandbox.io>
2022-09-03 20:11:32 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a9cc496416e74fc92a925a68ac16eaac.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ee967c920e8e68cfe488ca7c78ae4c4",
+    "url": "https://olusegun-light.github.io/csb-c1hc9q/index.html"
+  },
+  {
+    "revision": "ee6b5caac71271137dcb",
+    "url": "https://olusegun-light.github.io/csb-c1hc9q/static/js/2.79de99e3.chunk.js"
+  },
+  {
+    "revision": "f922b2d0f76d26bec24b",
+    "url": "https://olusegun-light.github.io/csb-c1hc9q/static/js/main.d63aef62.chunk.js"
+  },
+  {
+    "revision": "770c1c9960f0a29088c2",
+    "url": "https://olusegun-light.github.io/csb-c1hc9q/static/js/runtime-main.8da6feca.js"
+  }
+]);
